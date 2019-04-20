532a1672db6b44876ea31e1ee851bea92e2cd4de
Tianyuan Deng <wergelesn@missouri.edu>
2019-04-19 21:58:15 -0500

    Build model and add iventory.json

---
+++ JSON Decoding/JSON Decoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Decoding
+//
+//  Created by Deng tianyuan on 4/19/19.
+//  Copyright © 2019 Deng tianyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JSON Decoding/JSON DecodingTests/JSON_DecodingTests.swift
@@ -0,0 +1,34 @@
+//
+//  JSON_DecodingTests.swift
+//  JSON DecodingTests
+//
+//  Created by Deng tianyuan on 4/19/19.
+//  Copyright © 2019 Deng tianyuan. All rights reserved.
+//
+
+import XCTest
+@testable import JSON_Decoding
+
+class JSON_DecodingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
